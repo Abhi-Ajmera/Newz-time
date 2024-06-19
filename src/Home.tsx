5b9e5a8a1e65353c8157c597f5b850c85803f5ec
Abhishek Ajmera <abhishekajmera17@gmail.com>
2024-06-19 22:47:09 +0530

    dropdown added

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,14 @@
 import Card from "./components/Card";
+import Dropdown from "./components/Dropdown";
 
 const Home = () => {
 	return (
 		<>
 			<h1 className="text-5xl text-center mb-8">Latest News</h1>
+			<div className="flex items-center gap-4 mb-8 mx-8 justify-end">
+				<p className="text-lg">Select Category</p>
+				<Dropdown />
+			</div>
 			<div className="flex flex-wrap gap-8 justify-evenly">
 				<Card />
 				<Card />
