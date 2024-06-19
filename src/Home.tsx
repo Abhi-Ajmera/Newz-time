c950503ec2efbff50c6aab8037ec6d41426534bf
Abhishek Ajmera <abhishekajmera17@gmail.com>
2024-06-19 22:38:55 +0530

    footer done and card component added

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,18 @@
+import Card from "./components/Card";
+
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<>
+			<h1 className="text-5xl text-center mb-8">Latest News</h1>
+			<div className="flex flex-wrap gap-8 justify-evenly">
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
+		</>
+	);
 };
 export default Home;
