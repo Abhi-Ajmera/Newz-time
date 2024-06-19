5a1bf84981ae76574f01496fcfad05e64e85e938
Abhishek Ajmera <abhishekajmera17@gmail.com>
2024-06-19 12:57:10 +0530

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<div className="min-h-screen bg-gray-800 text-white px-6 py-2">
+			<h1 className="text-2xl">Hello</h1>
+		</div>
+	</React.StrictMode>
+);
